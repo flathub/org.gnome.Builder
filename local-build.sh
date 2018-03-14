1fe7201f88afb924c47f59338899f6907d683059
Christian Hergert <chergert@redhat.com>
2018-03-14 14:38:29 -0700

    add script to test build

---
+++ local-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -f org.gnome.Builder.flatpak
+rm -rf _build ; mkdir _build
+rm -rf _repo ; mkdir _repo
+
+flatpak-builder --ccache --force-clean _build org.gnome.Builder.json --repo=_repo
+flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder master
+
