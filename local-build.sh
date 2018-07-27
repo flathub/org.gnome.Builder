23b6b88d5ce59bae00f16d50f168cf548ae2fd5c
Christian Hergert <chergert@redhat.com>
2018-07-27 12:50:10 -0700

    build: use stable for branch name

--- local-build.sh
+++ local-build.sh
@@ -4,6 +4,6 @@ rm -f org.gnome.Builder.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build org.gnome.Builder.json --repo=_repo
-flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder master
+flatpak-builder --ccache --force-clean _build org.gnome.Builder.json --repo=_repo --default-branch=stable
+flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder stable
 
