1ce9b452a7388d4568900ab469b54e01340db7c9
Christian Hergert <chergert@redhat.com>
2019-09-12 13:57:42 -0700

    release 3.34.0

--- local-build.sh
+++ local-build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+BRANCH=stable
+STATE_DIR="--state-dir=${HOME}/.cache/gnome-builder/flatpak-builder/"
+
 rm -f org.gnome.Builder.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build org.gnome.Builder.json --repo=_repo --default-branch=stable
-flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder stable
+flatpak-builder "${STATE_DIR}" --ccache --force-clean _build org.gnome.Builder.json --repo=_repo "--default-branch=${BRANCH}"
+flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder "${BRANCH}"
 
